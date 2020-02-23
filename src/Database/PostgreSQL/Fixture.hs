e4672d9880f2d740d8c85fc79d933b6c5e16adfe
Gavin Panella <gavinpanella@gmail.com>
2020-02-23 22:30:20 +0100

    Move ephemeralCluster into a submodule.

--- src/Database/PostgreSQL/Fixture.hs
+++ src/Database/PostgreSQL/Fixture.hs
@@ -1,43 +1,14 @@
--- |
--- Description : Fixtures for bringing up PostgreSQL clusters.
 module Database.PostgreSQL.Fixture
   ( ephemeralCluster,
     simpleConnection,
   )
 where
 
-import Control.Monad.IO.Class (liftIO)
 import qualified Data.Acquire
-import qualified Database.PostgreSQL.Fixture.Cluster as Cluster
-import Database.PostgreSQL.Fixture.Cluster (Cluster (Cluster))
+import Database.PostgreSQL.Fixture.Ephemeral (ephemeralCluster)
 import qualified Database.PostgreSQL.Fixture.Settings as Settings
 import qualified Database.PostgreSQL.Simple as Simple
 import qualified Database.PostgreSQL.Simple.Types as Simple.Types
-import System.Directory (getTemporaryDirectory, removeDirectoryRecursive)
-import qualified System.Environment as Environment
-import System.FilePath ((</>))
-import System.Posix.Temp (mkdtemp)
-import qualified System.Process.Typed as Process
-import Text.Printf (printf)
-
-ephemeralCluster :: Data.Acquire.Acquire Settings.ConnectionSettings
-ephemeralCluster = ephemeralCluster' []
-
-ephemeralCluster' :: [(String, String)] -> Data.Acquire.Acquire Settings.ConnectionSettings
-ephemeralCluster' extraEnv = do
-  tempDir <- temporaryDirectory
-  let dataDir = tempDir </> "cluster"
-  let cluster = Cluster dataDir extraEnv
-  Data.Acquire.mkAcquire (acquire cluster) (release cluster)
-  where
-    acquire :: Cluster -> IO Settings.ConnectionSettings
-    acquire cluster = do
-      Cluster.create cluster
-      Cluster.start cluster
-    release :: Cluster -> Settings.ConnectionSettings -> IO ()
-    release cluster _ = do
-      Cluster.stop cluster
-      Cluster.destroy cluster
 
 simpleConnection :: Settings.ConnectionSettings -> Data.Acquire.Acquire Simple.Connection
 simpleConnection
@@ -62,14 +33,3 @@ simpleConnection
       release :: Simple.Connection -> IO ()
       release =
         Simple.close
-
-temporaryDirectory :: Data.Acquire.Acquire FilePath
-temporaryDirectory = do
-  -- NOTE: keep the temporary path short because UNIX domain socket paths are
-  -- limited to 103 bytes on macOS (maybe it's different on Linux) and macOS
-  -- likes to set $TMP to /var/folders/83/p3y5dr1s0zj0w95s7t_mzwcc0000gk/T/ or
-  -- something bizarre like that.
-  tmp <- liftIO getTemporaryDirectory
-  Data.Acquire.mkAcquire
-    (mkdtemp (tmp </> "pg."))
-    removeDirectoryRecursive
---
+++ src/Database/PostgreSQL/Fixture/Ephemeral.hs
@@ -0,0 +1,43 @@
+module Database.PostgreSQL.Fixture.Ephemeral
+  ( ephemeralCluster,
+  )
+where
+
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Acquire
+import qualified Database.PostgreSQL.Fixture.Cluster as Cluster
+import Database.PostgreSQL.Fixture.Cluster (Cluster (Cluster))
+import Database.PostgreSQL.Fixture.Settings (ConnectionSettings)
+import System.Directory (getTemporaryDirectory, removeDirectoryRecursive)
+import System.FilePath ((</>))
+import System.Posix.Temp (mkdtemp)
+
+ephemeralCluster :: Data.Acquire.Acquire ConnectionSettings
+ephemeralCluster = ephemeralCluster' []
+
+ephemeralCluster' :: [(String, String)] -> Data.Acquire.Acquire ConnectionSettings
+ephemeralCluster' extraEnv = do
+  tempDir <- temporaryDirectory
+  let dataDir = tempDir </> "cluster"
+  let cluster = Cluster dataDir extraEnv
+  Data.Acquire.mkAcquire (acquire cluster) (release cluster)
+  where
+    acquire :: Cluster -> IO ConnectionSettings
+    acquire cluster = do
+      Cluster.create cluster
+      Cluster.start cluster
+    release :: Cluster -> ConnectionSettings -> IO ()
+    release cluster _ = do
+      Cluster.stop cluster
+      Cluster.destroy cluster
+
+temporaryDirectory :: Data.Acquire.Acquire FilePath
+temporaryDirectory = do
+  -- NOTE: keep the temporary path short because UNIX domain socket paths are
+  -- limited to 103 bytes on macOS (maybe it's different on Linux) and macOS
+  -- likes to set $TMP to /var/folders/83/p3y5dr1s0zj0w95s7t_mzwcc0000gk/T/ or
+  -- something bizarre like that.
+  tmp <- liftIO getTemporaryDirectory
+  Data.Acquire.mkAcquire
+    (mkdtemp (tmp </> "pg."))
+    removeDirectoryRecursive
