0917ec91b635d69b4f6a75d3fd78e26165e740c7
Gavin Panella <gavinpanella@gmail.com>
2020-02-29 21:03:17 +0100

    Fix a few more warnings.

--- src/Database/PostgreSQL/Fixture/Consumers.hs
+++ src/Database/PostgreSQL/Fixture/Consumers.hs
@@ -9,7 +9,7 @@ where
 
 import Control.Exception (catchJust)
 import Foreign.C.Error (Errno (Errno), eNOTEMPTY)
-import GHC.IO.Exception (IOErrorType (UnsatisfiedConstraints), IOException (ioe_errno))
+import GHC.IO.Exception (IOException (ioe_errno))
 import qualified System.Directory as Directory
 import System.FilePath ((</>), FilePath)
 import System.IO.Error (isDoesNotExistError)
@@ -28,18 +28,18 @@ data Consumer
       }
 
 acquire :: Consumer -> IO ()
-acquire consumer@Consumer {lockDir, consumerType} = do
+acquire consumer@Consumer {lockDir} = do
   Directory.createDirectoryIfMissing True lockDir
-  lockFilePath <- lockFilePath consumer
-  writeFile lockFilePath ""
+  lockFilePath_ <- lockFilePath consumer
+  writeFile lockFilePath_ ""
 
 release :: Consumer -> IO ()
 release consumer = do
   catchJust predicate removeLockFile pure
   where
     removeLockFile = do
-      lockFilePath <- lockFilePath consumer
-      Directory.removeFile lockFilePath
+      lockFilePath_ <- lockFilePath consumer
+      Directory.removeFile lockFilePath_
     predicate e
       | isDoesNotExistError e = Just ()
       | otherwise = Nothing
